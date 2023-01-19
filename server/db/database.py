c3437eeaf7c8797a2992f23a1a4a844731859435
Nikhil Narasimhan <Nikhil.Narasimhan@axisbank.com>
2023-01-20 00:52:48 +0530

    Made db class a singleton.

--- server/db/database.py
+++ server/db/database.py
@@ -3,30 +3,34 @@ import json
 
 
 class Database:
+
+    __db = dict()
+
     def __init__(self) -> None:
         # currently loads registries from the file
         # in the future when the db is implemented:
         #   - connect to db and get database object
         #   - optionally create collections
 
-        self.db = dict()
+        if self.__db != {}:
+            return
 
         base_path = os.path.dirname(__file__)
         with open(base_path + "/fixtures/models_registry.json") as fhand:
             models_registry = json.loads(fhand.read())
-            self.db["model"] = models_registry
+            self.__db["model"] = models_registry
 
         with open(base_path + "/fixtures/services_registry.json") as fhand:
             services_registry = json.loads(fhand.read())
-            self.db["service"] = services_registry
+            self.__db["service"] = services_registry
 
         with open(base_path + "/fixtures/api_key_registry.json") as fhand:
-            services_registry = json.loads(fhand.read())
-            self.db["api_key"] = services_registry
+            api_key_registry = json.loads(fhand.read())
+            self.__db["api_key"] = api_key_registry
 
     # this function is temporarily created until db is implemented
     def __getitem__(self, key: str):
-        return self.db[key]
+        return self.__db[key]
 
     # def load_collections(self):
     #     from .BaseRepository import BaseRepository
