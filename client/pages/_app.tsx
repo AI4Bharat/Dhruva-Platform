2bcf49b0e8758906142f808da4b385678711b8c4
Shanks0465 <umashanks99@gmail.com>
2023-01-20 19:41:09 +0530

    Removed Indic Transliterate Package

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,7 +6,6 @@ import Sidebar from "../components/Navigation/Sidebar";
 import { useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import "../styles/global.css";
-import "../node_modules/@ai4bharat/indic-transliterate/dist/index.css";
 import Navbar from "../components/Navigation/Navbar";
 import NavbarMobile from "../components/Navigation/NavbarMobile";
 
