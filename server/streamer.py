933f23d3160075efd6c8fe00b97756df975fca10
Ubuntu <gokulnc@ymail.com>
2023-01-25 15:24:53 +0000

    [streaming] minor refactor

--- server/streamer.py
+++ server/streamer.py
@@ -11,7 +11,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @dataclass
-class UserState:
+class UserStateForASR:
     buffer: bytes
     language: str
     sampling_rate: int
@@ -22,48 +22,43 @@ class UserState:
     last_inference_position_in_bytes: int
 
 class StreamingServerASR:
-    def __init__(self, app, response_frequency_in_ms=2000, bytes_per_sample=2) -> None:
+    '''
+    This is a SocketIO server for simulating streaming-ASR.
+    TODO: Write a base class or wrapper above this, that generalizes across task-types
+
+    The following implementation is based on the standards followed by Vakyansh's Open-Speech-API:
+    https://github.com/AI4Bharat/speech-recognition-open-api-proxy
+    '''
+    def __init__(self, response_frequency_in_ms: int = 2000, bytes_per_sample: int = 2) -> None:
         self.sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
         self.app = socketio.ASGIApp(
             self.sio, socketio_path="",
             # other_asgi_app=app
         )
-        app.mount("/socket.io", self.app)
-        self.configure_socket_server()
-        self.client_states = {}
 
+        # Save config
         self.response_frequency_in_ms = response_frequency_in_ms
         self.bytes_per_sample = bytes_per_sample
+        
+        # Storage for state specific to each client (key will be socket connection-ID string, and value would be `UserStateForASR`)
+        self.client_states = {}
+
+        # Setup the communication handlers
+        self.configure_socket_server()
     
-    def delete_user_states(self, sid):
+    def delete_user_states(self, sid: str) -> None:
         self.client_states.pop(sid, None)
     
-    def initialize_buffer(self, sid):
+    def initialize_buffer(self, sid: str) -> None:
         self.client_states[sid].buffer = bytes()
         self.client_states[sid].last_inference_position_in_bytes = 0
     
-    async def transcribe_and_send(self, sid):
-        if not self.client_states[sid].buffer:
-            return
-        message = self.run_inference(sid)
-        await self.sio.emit(
-            "response",
-            data=(message, self.client_states[sid].language),
-            room=sid
-        )
-
-    def run_inference(self, sid):
-        byte_io = io.BytesIO()
-        with wave.open(byte_io, "wb") as file:
-            file.setnchannels(1)
-            file.setsampwidth(2)
-            file.setframerate(self.client_states[sid].sampling_rate)
-            file.writeframes(self.client_states[sid].buffer)
-        
-        byte_io.seek(0)
-        encoded_bytes = base64.b64encode(byte_io.read())
+    def run_ulca_inference_from_stream(self, sid: str, stream: io.IOBase) -> str:
+        # Convert the byte-stream into base64 string
+        encoded_bytes = base64.b64encode(stream.read())
         encoded_string = encoded_bytes.decode()
-        print("Running inference")
+
+        # Construct ULCA request payload
         request_json = {
             "serviceId": self.client_states[sid].service_id,
             "audio": [
@@ -81,11 +76,12 @@ class StreamingServerASR:
             }
         }
         
+        # Run inference via Dhruva REST API
         response = requests.post(
             "https://api.dhruva.ai4bharat.org/services/inference/asr",
             json=request_json,
             headers={"authorization": self.client_states[sid].api_key},
-            timeout=1
+            # timeout=1
         )
         # print("response.text", response.text)
         response_json = response.json()
@@ -95,43 +91,79 @@ class StreamingServerASR:
             print(response.text)
             return "<!--ERROR-->"
 
+    def run_inference(self, sid: str) -> str:
+        # Convert the raw bytes to WAV format, and create a byte-stream
+        byte_io = io.BytesIO()
+        with wave.open(byte_io, "wb") as file:
+            file.setnchannels(1)
+            file.setsampwidth(2)
+            file.setframerate(self.client_states[sid].sampling_rate)
+            file.writeframes(self.client_states[sid].buffer)
+        
+        byte_io.seek(0)
+        return self.run_ulca_inference_from_stream(sid, byte_io)
+    
+    async def transcribe_and_send(self, sid: str) -> None:
+        if not self.client_states[sid].buffer:
+            return
+        message = self.run_inference(sid)
+        await self.sio.emit(
+            "response",
+            data=(message, self.client_states[sid].language),
+            room=sid
+        )
+
     def configure_socket_server(self):
         @self.sio.event
-        def connect(sid, environ, auth):
+        async def connect(sid: str, environ: dict, auth):
             print('Connected with:', sid)
             query_dict = parse_qs(environ["QUERY_STRING"])
             sampling_rate = int(query_dict["samplingRate"][0])
-            self.client_states[sid] = UserState(
+
+            if False: # TODO: Validate the fields: api_key, service_id, language
+                await self.sio.emit("abort", room=sid)
+                return False
+            
+            # Compute the inference_frequency (once in how many bytes should we run inference)
+            run_inference_once_in_bytes = int(sampling_rate * (self.response_frequency_in_ms / 1000) * self.bytes_per_sample)
+
+            self.client_states[sid] = UserStateForASR(
                 buffer=None,
                 language=query_dict["language"][0],
                 sampling_rate=sampling_rate,
-                post_processors=json.loads(query_dict["postProcessors"][0]),
+                post_processors=json.loads(query_dict["postProcessors"][0]) if "postProcessors" in query_dict else [],
                 service_id=query_dict["serviceId"][0],
                 api_key=query_dict["apiKey"][0],
-                run_inference_once_in_bytes=int(sampling_rate * (self.response_frequency_in_ms / 1000) * self.bytes_per_sample),
+                run_inference_once_in_bytes=run_inference_once_in_bytes,
                 last_inference_position_in_bytes=0
             )
+            return True
         
         @self.sio.on("connect_mic_stream")
-        async def connect_mic_stream(sid):
-            print("Connected stream")
+        async def connect_mic_stream(sid: str):
             self.initialize_buffer(sid)
+            # print("Connected stream for:", sid)
             await self.sio.emit("connect-success", room=sid)
         
         @self.sio.on("mic_data")
         async def mic_data(sid, in_data: bytes, language_code: str, is_speaking: bool, disconnect_stream: bool):
-            if in_data:
+            if in_data: # Append audio payload to client buffer
                 self.client_states[sid].buffer += in_data
+            
             if not is_speaking:
+                # If silence is detected, run inference once (in-case there was new data after previous inference) and clear the buffer
                 await self.transcribe_and_send(sid)
                 self.initialize_buffer(sid)
             else:
+                # Run inference once we have accumulated enough amount of audio since previous inference
                 if len(self.client_states[sid].buffer) - self.client_states[sid].last_inference_position_in_bytes >= self.client_states[sid].run_inference_once_in_bytes:
                     await self.transcribe_and_send(sid)
                     self.client_states[sid].last_inference_position_in_bytes = len(self.client_states[sid].buffer)
             
             if disconnect_stream:
+                # If the client wants to disconnect from the stream, run inference for one last-time (in-case there was new data after previous inference)
                 await self.transcribe_and_send(sid)
+                # Remove all info related to the connection, and issue an handshake-signal to terminate
                 self.delete_user_states(sid)
                 await self.sio.emit("terminate", room=sid)
         
@@ -140,4 +172,7 @@ class StreamingServerASR:
             self.delete_user_states(sid)
             # print("Disconnected with:", sid)
 
-StreamingServerASR(app)
+streamer = StreamingServerASR()
+
+# Mount it at the default path of SocketIO engine
+app.mount("/socket.io", streamer.app)
