d43b7e458d44c64fb3a836d388d89fb2fc75addd
Yash Kale <yashkale@Yashs-MacBook-Air.local>
2023-01-26 16:27:56 +0530

    independed search added

--- client/pages/services/index.tsx
+++ client/pages/services/index.tsx
@@ -33,218 +33,273 @@ interface Service {
   hardwareDescription: string;
   publishedOn: number;
   modelId: string;
-  task_type : any;
-  languages : any;
+  task_type: any;
+  languages: any;
 }
 
 export default function Services() {
   const [services, setServices] = useState<Service[]>([]);
-  const [sourceLang, setSourceLanguage] = useState<String>("")
-  const [targetLang, setTargetLanguage] = useState<String>("")
+  const [sourceLang, setSourceLanguage] = useState<String>("");
+  const [targetLang, setTargetLanguage] = useState<String>("");
   const [filteredservices, setFilteredServices] = useState<Service[]>([]);
-  const [hide, togglehide] = useState<boolean>(true)
+  const [hide, togglehide] = useState<boolean>(true);
   const smallscreen = useMediaQuery("(max-width: 1080px)");
 
-  const servicesToggler = (event:any) => 
-  {
+  const servicesToggler = (event: any) => {
     setFilteredServices(
       services.filter((service) =>
-        service.name
-          .toLowerCase()
-          .includes(event.target.value.toLowerCase())
+        service.name.toLowerCase().includes(event.target.value.toLowerCase())
       )
     );
   };
 
-  const taskToggler = (event:any) =>
-  {
+  const taskToggler = (event: any) => {
     setFilteredServices(
       services.filter((service) =>
-        service.task_type.type
-          .includes(event.target.value)
+        service.task_type.type.includes(event.target.value)
       )
     );
-  }
+  };
 
-  const langToggler = (src : String, target: String) =>
-  {
-    if(src="")
-    {
-      setFilteredServices(
-        services.filter((service) =>
-        service.languages.forEach((p:any)=>{
-          p.targetLanguage.includes(target)
-        })
-        )
-      );
-    }
-    if(target="")
-    {
-      setFilteredServices(
-        services.filter((service) =>
-        service.languages.some((p:any)=>{
-          p.sourceLanguage.includes(src)
-        })
-        )
-      );
-    }
-    else
-    {
-      setFilteredServices(
-        services.filter((service) =>
-        service.languages.some((p:any)=>{
-          p.sourceLanguage.includes(src) &&
-          p.targetLanguage.includes(target)
-        })
-        )
-      );
-    }
-  }
-  const sourceLangToggler = (event:any) =>
-  {
+  const langToggler = (src: String, target: String) => {
+    setFilteredServices(
+      services.filter((service) => {
+        let found = false;
+        if (target === "" && src !== "") {
+          service.languages.every(
+            (language: { sourceLanguage: String; targetLanguage: String }) => {
+              if (language.sourceLanguage === src) {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        } else if (src === "" && target !== "") {
+          service.languages.every(
+            (language: { sourceLanguage: String; targetLanguage: String }) => {
+              if (language.targetLanguage === target) {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        } else if (target !== "" && src !== "") {
+          service.languages.every(
+            (language: { sourceLanguage: String; targetLanguage: String }) => {
+              if (
+                language.targetLanguage === target &&
+                language.sourceLanguage === src
+              ) {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        }
+        return found;
+      })
+    );
+  };
+  const sourceLangToggler = (event: any) => {
     setSourceLanguage(event.target.value);
-    langToggler(sourceLang,targetLang);
-  } 
+  };
 
-  const targetLangToggler = (event:any) =>
-  {
+  const targetLangToggler = (event: any) => {
     setTargetLanguage(event.target.value);
-    langToggler(sourceLang,targetLang);
-  }
+  };
 
   useEffect(() => {
     axios({
       method: "GET",
       url: dhruvaConfig.listServices,
-    }).then((response) => {setServices(response.data); setFilteredServices(response.data); togglehide(false)});
+    }).then((response) => {
+      setServices(response.data);
+      setFilteredServices(response.data);
+      togglehide(false);
+    });
   }, []);
+  useEffect(() => {
+    langToggler(sourceLang, targetLang);
+  }, [sourceLang, targetLang]);
 
   return (
     <>
       <ContentLayout>
-        <Box
-          bg="light.100"
-          ml={smallscreen ? "1rem" : "0rem"}
-        >
+        <Box bg="light.100" ml={smallscreen ? "1rem" : "0rem"}>
           {/* Searchbar */}
-          <Stack direction={['column', 'row']} spacing='0.5rem' background={"gray.50"}>
-          <InputGroup width={smallscreen ? "90vw" : "30rem"} background={"white"}>
-            <InputLeftElement
+          <Stack
+            direction={["column", "row"]}
+            spacing="0.5rem"
+            background={"gray.50"}
+          >
+            <InputGroup
+              width={smallscreen ? "90vw" : "30rem"}
+              background={"white"}
+            >
+              <InputLeftElement
+                color="gray.300"
+                pointerEvents="none"
+                children={<IoSearchOutline />}
+              />
+              <Input
+                borderRadius={0}
+                onChange={servicesToggler}
+                placeholder="Search for Services"
+              />
+            </InputGroup>
+            <Select
+              width={smallscreen ? "90vw" : "20rem"}
+              background={"white"}
+              borderRadius={0}
               color="gray.300"
-              pointerEvents="none"
-              children={<IoSearchOutline />}
-            />
-            <Input borderRadius={0} onChange={servicesToggler} placeholder="Search for Services" />
-          </InputGroup>
-          <Select width={smallscreen ? "90vw" : "20rem"} background={"white"} borderRadius={0} color="gray.300" onChange={taskToggler}>
-            <option  hidden defaultChecked>Select Task Type</option>
-            <option value='translation'>Translation</option>
-            <option value='tts'>TTS</option>
-            <option value='asr'>ASR</option>
-          </Select>
-          <InputGroup width={smallscreen ? "90vw" : "30rem"} background={"white"}>
-          <Select background={"white"} borderRadius={0} color="gray.300" onChange={sourceLangToggler}>
-            <option  hidden defaultChecked>Source Language</option>
-            <option value='en'>English</option>
-            <option value='hi'>Hindi</option>
-          </Select>
-          <Select background={"white"} borderRadius={0} color="gray.300" onChange={targetLangToggler}>
-            <option  hidden defaultChecked>Target Language</option>
-            <option value='en'>English</option>
-            <option value='hi'>Hindi</option>
-            <option value='as'>Assamese</option>
-            <option value='bn'>Bengali</option>
-            <option value='gu'>Gujarati</option>
-            <option value='kn'>Kannada</option>
-            <option value='ml'>Malayalam</option>
-            <option value='mr'>Marathi</option>
-            <option value='or'>Oriya</option>
-            <option value='pa'>Punjabi</option>
-            <option value='ta'>Tamil</option>            
-            <option value='te'>Telugu</option>
-          </Select>
-          </InputGroup>
+              onChange={taskToggler}
+            >
+              <option hidden defaultChecked>
+                Select Task Type
+              </option>
+              <option value="translation">Translation</option>
+              <option value="tts">TTS</option>
+              <option value="asr">ASR</option>
+            </Select>
+            <InputGroup
+              width={smallscreen ? "90vw" : "30rem"}
+              background={"white"}
+            >
+              <Select
+                background={"white"}
+                borderRadius={0}
+                color="gray.300"
+                onChange={sourceLangToggler}
+              >
+                <option hidden defaultChecked>
+                  Source Language
+                </option>
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+              </Select>
+              <Select
+                background={"white"}
+                borderRadius={0}
+                color="gray.300"
+                onChange={targetLangToggler}
+              >
+                <option hidden defaultChecked>
+                  Target Language
+                </option>
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+                <option value="as">Assamese</option>
+                <option value="bn">Bengali</option>
+                <option value="gu">Gujarati</option>
+                <option value="kn">Kannada</option>
+                <option value="ml">Malayalam</option>
+                <option value="mr">Marathi</option>
+                <option value="or">Oriya</option>
+                <option value="pa">Punjabi</option>
+                <option value="ta">Tamil</option>
+                <option value="te">Telugu</option>
+              </Select>
+            </InputGroup>
           </Stack>
         </Box>
         <br />
         {smallscreen ? (
           // Mobile View
-          filteredservices.length > 0?
-          <>
-            {Object.entries(filteredservices).map(([id, serviceData]) => (
-              <ServiceCard
-                key={id}
-                name={serviceData.name}
-                serviceID={serviceData.serviceId}
-                modelID={serviceData.modelId}
-                date={new Date(serviceData.publishedOn).toDateString()}
-              />
-            ))}
-          </>
-          :
-          <>
-            <HStack background={"gray.50"} width="100vw" height="50vh">
-            <Spacer/>
-              <Box textAlign={"center"} display={hide?"none":"block"} >
-              <Image height={300} width={300}  alt="No Results Found" src="NoResults.svg"/>
-              <Text fontSize={"lg"} color="gray.400">Uh Oh! No Results Found</Text>
-              </Box>
-            <Spacer/>
-          </HStack>
-          </>
+          filteredservices.length > 0 ? (
+            <>
+              {Object.entries(filteredservices).map(([id, serviceData]) => (
+                <ServiceCard
+                  key={id}
+                  name={serviceData.name}
+                  serviceID={serviceData.serviceId}
+                  modelID={serviceData.modelId}
+                  date={new Date(serviceData.publishedOn).toDateString()}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              <HStack background={"gray.50"} width="100vw" height="50vh">
+                <Spacer />
+                <Box textAlign={"center"} display={hide ? "none" : "block"}>
+                  <Image
+                    height={300}
+                    width={300}
+                    alt="No Results Found"
+                    src="NoResults.svg"
+                  />
+                  <Text fontSize={"lg"} color="gray.400">
+                    Uh Oh! No Results Found
+                  </Text>
+                </Box>
+                <Spacer />
+              </HStack>
+            </>
+          )
         ) : (
           // Desktop View
           <Box bg="light.100">
-            {(filteredservices.length>0)?            
-            <Table variant="unstyled">
-              <Thead>
-                <Tr>
-                  <Th>Name</Th>
-                  <Th>Service ID</Th>
-                  <Th>Model ID</Th>
-                  <Th>Published On</Th>
-                  <Th>Actions</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {Object.entries(filteredservices).map(([id, serviceData]) => {
-                  const publishedOn = new Date(serviceData.publishedOn);
-                  return (
-                    <Tr key={id} fontSize={"sm"}>
-                      <Td>{serviceData.name}</Td>
-                      <Td>{serviceData.serviceId}</Td>
-                      <Td>{serviceData.modelId}</Td>
-                      <Td>{publishedOn.toDateString()}</Td>
-                      <Td>
-                        {" "}
-                        <Link
-                          href={{
-                            pathname: `/services/view`,
-                            query: {
-                              serviceId: serviceData.serviceId,
-                            },
-                          }}
-                        >
+            {filteredservices.length > 0 ? (
+              <Table variant="unstyled">
+                <Thead>
+                  <Tr>
+                    <Th>Name</Th>
+                    <Th>Service ID</Th>
+                    <Th>Model ID</Th>
+                    <Th>Published On</Th>
+                    <Th>Actions</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {Object.entries(filteredservices).map(([id, serviceData]) => {
+                    const publishedOn = new Date(serviceData.publishedOn);
+                    return (
+                      <Tr key={id} fontSize={"sm"}>
+                        <Td>{serviceData.name}</Td>
+                        <Td>{serviceData.serviceId}</Td>
+                        <Td>{serviceData.modelId}</Td>
+                        <Td>{publishedOn.toDateString()}</Td>
+                        <Td>
                           {" "}
-                          <Button size={"sm"} variant={"outline"}>
-                            View
-                          </Button>
-                        </Link>
-                      </Td>
-                    </Tr>
-                  );
-                })}
-              </Tbody>
-            </Table>:
-            <HStack background={"gray.50"}>
-            <Spacer/>
-            <Box textAlign={"center"} display={hide?"none":"block"}>
-              <Image height={400} width={400}  alt="No Results Found" src="NoResults.svg"/>
-              <Text fontSize={"lg"} color="gray.400">Uh Oh! No Results Found</Text>
-            </Box>
-            <Spacer/>
-            </HStack>
-            }
+                          <Link
+                            href={{
+                              pathname: `/services/view`,
+                              query: {
+                                serviceId: serviceData.serviceId,
+                              },
+                            }}
+                          >
+                            {" "}
+                            <Button size={"sm"} variant={"outline"}>
+                              View
+                            </Button>
+                          </Link>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            ) : (
+              <HStack background={"gray.50"}>
+                <Spacer />
+                <Box textAlign={"center"} display={hide ? "none" : "block"}>
+                  <Image
+                    height={400}
+                    width={400}
+                    alt="No Results Found"
+                    src="NoResults.svg"
+                  />
+                  <Text fontSize={"lg"} color="gray.400">
+                    Uh Oh! No Results Found
+                  </Text>
+                </Box>
+                <Spacer />
+              </HStack>
+            )}
           </Box>
         )}
       </ContentLayout>
