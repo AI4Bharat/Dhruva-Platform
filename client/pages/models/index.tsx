b61dc8ea5f47d7e01025bd751a9bf43c0255da97
Yash Kale <yashkale@Yashs-MacBook-Air.local>
2023-01-28 15:48:55 +0530

    Filtering for Models Page done

--- client/pages/models/index.tsx
+++ client/pages/models/index.tsx
@@ -5,7 +5,9 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Select,
   Spacer,
+  Stack,
   Table,
   Tbody,
   Td,
@@ -29,55 +31,214 @@ interface Model {
   version: string;
   modelId: string;
   task: any;
+  languages: any;
 }
 export default function Models() {
   const [models, setModels] = useState<Model[]>([]);
   const [hide, togglehide] = useState<boolean>(true)
+  const [sourceLang, setSourceLanguage] = useState<String>("");
+  const [targetLang, setTargetLanguage] = useState<String>("");
+  const [task, setTask] = useState<String>("");
   const smallscreen = useMediaQuery("(max-width: 1080px)");
   const [filteredModels, setFilteredModels] = useState<Model[]>(models)
-
-  const modelsToggler = (event:any) => 
+  const [searchedModels, setSearchedModels] = useState<Model[]>([]);
+  const [seed, setSeed] = useState<number>(0)
+  
+  const clearFilters = () =>
   {
-    setFilteredModels(
-      models.filter((model) =>
-        model.name
-          .toLowerCase()
-          .includes(event.target.value.toLowerCase())
+    setTask("");
+    setSeed(Math.random());
+    setSourceLanguage("");
+    setTargetLanguage("");
+    setFilteredModels(models);
+    setSearchedModels(models);
+  }
+
+  const searchToggler = (event: any) => {
+    setSearchedModels(
+      filteredModels.filter((model) =>
+        model.name.toLowerCase().includes(event.target.value.toLowerCase())
       )
     );
   };
+
+  const filterToggler = () => 
+  {
+    if(task !== "" || sourceLang!=="" || targetLang!=="")
+    setFilteredModels
+    (
+      models.filter((model) => 
+      {
+        let found = false;
+        if (targetLang === "" && sourceLang !== "") 
+        {
+          model.languages.every((language: { sourceLanguage: String; targetLanguage: String }) => 
+            {
+              if (language.sourceLanguage === sourceLang) 
+              {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        } 
+        else if (sourceLang === "" && targetLang !== "") 
+        {
+          model.languages.every((language: { sourceLanguage: String; targetLanguage: String }) => 
+            {
+              if (language.targetLanguage === targetLang) 
+              {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        } 
+        else if (targetLang !== "" && sourceLang !== "") 
+        {
+          model.languages.every((language: { sourceLanguage: String; targetLanguage: String }) => 
+            {
+              if (
+                language.targetLanguage === targetLang &&
+                language.sourceLanguage === sourceLang
+              ) 
+              {
+                found = true;
+                return false;
+              }
+              return true;
+            }
+          );
+        }
+        else if (targetLang === "" && sourceLang === "" && task !== "")
+        {
+          return model.task.type.includes(task);
+        } 
+        return found && model.task.type.includes(task)
+      }
+      )
+    );
+  };
+
+  const sourceLangToggler = (event: any) => {
+    setSourceLanguage(event.target.value);
+  };
+
+  const targetLangToggler = (event: any) => {
+    setTargetLanguage(event.target.value);
+  };
+
+  const taskToggler = (event: any) => {
+    setTask(event.target.value);
+  };
+
+  useEffect(() => {
+    filterToggler();
+  }, [sourceLang, targetLang, task]);
+
+
+  useEffect(() => {
+    setSearchedModels(filteredModels)
+  }, [filteredModels]);
 
   useEffect(() => {
     axios({
       method: "GET",
       url: dhruvaConfig.listModels,
-    }).then((response) => {setModels(response.data); setFilteredModels(response.data); togglehide(false)});
+    }).then((response) => {setModels(response.data); setFilteredModels(response.data); setSearchedModels(response.data); togglehide(false)});
   }, []);
 
   return (
     <>
       <ContentLayout>
-        <Box
-          width={smallscreen ? "90vw" : "30rem"}
-          bg="light.100"
-          ml={smallscreen ? "1rem" : "0rem"}
-        >
+      <Box bg="light.100" ml={smallscreen ? "1rem" : "0rem"} key={seed}>
           {/* Searchbar */}
-          <InputGroup>
-            <InputLeftElement
+          <Stack
+            direction={["column", "row"]}
+            spacing="0.5rem"
+            background={"gray.50"}
+          >
+            <InputGroup
+              width={smallscreen ? "90vw" : "30rem"}
+              background={"white"}
+            >
+              <InputLeftElement
+                color="gray.300"
+                pointerEvents="none"
+                children={<IoSearchOutline />}
+              />
+              <Input
+                borderRadius={0}
+                onChange={searchToggler}
+                placeholder="Search for Services"
+              />
+            </InputGroup>
+            <Select
+              width={smallscreen ? "90vw" : "20rem"}
+              background={"white"}
+              borderRadius={0}
               color="gray.300"
-              pointerEvents="none"
-              children={<IoSearchOutline />}
-            />
-            <Input onChange={modelsToggler} borderRadius={0} placeholder="Search for Models" />
-          </InputGroup>
+              onChange={taskToggler}
+            >
+              <option selected hidden>
+                Select Task Type
+              </option>
+              <option value="translation">Translation</option>
+              <option value="tts">TTS</option>
+              <option value="asr">ASR</option>
+            </Select>
+            <InputGroup
+              width={smallscreen ? "90vw" : "30rem"}
+              background={"white"}
+            >
+              <Select
+                background={"white"}
+                borderRadius={0}
+                color="gray.300"
+                onChange={sourceLangToggler}
+              >
+                <option hidden defaultChecked>
+                  Source Language
+                </option>
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+              </Select>
+              <Select
+                background={"white"}
+                borderRadius={0}
+                color="gray.300"
+                onChange={targetLangToggler}
+              >
+                <option hidden defaultChecked>
+                  Target Language
+                </option>
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+                <option value="as">Assamese</option>
+                <option value="bn">Bengali</option>
+                <option value="gu">Gujarati</option>
+                <option value="kn">Kannada</option>
+                <option value="ml">Malayalam</option>
+                <option value="mr">Marathi</option>
+                <option value="or">Oriya</option>
+                <option value="pa">Punjabi</option>
+                <option value="ta">Tamil</option>
+                <option value="te">Telugu</option>
+              </Select>
+            </InputGroup>
+            <Button width={smallscreen?"90vw":"8rem"} onClick={clearFilters}>
+              Clear Filters
+            </Button>
+          </Stack>
         </Box>
         <br />
         {smallscreen ? ( 
           // Mobile View
-          filteredModels.length > 0 ? 
+          searchedModels.length > 0 ? 
           <Box>
-            {Object.entries(filteredModels).map(([id, modelData]) => (
+            {Object.entries(searchedModels).map(([id, modelData]) => (
               <ModelCard
                 key={id}
                 name={modelData.name}
@@ -100,7 +261,7 @@ export default function Models() {
         ) : (
           // Desktop View
           <Box bg="light.100">
-            {filteredModels.length > 0 ?            
+            {searchedModels.length > 0 ?            
             <Table variant="unstyled">
               <Thead>
                 <Tr>
@@ -112,7 +273,7 @@ export default function Models() {
                 </Tr>
               </Thead>
               <Tbody>
-                {Object.entries(filteredModels).map(([id, modelData]) => {
+                {Object.entries(searchedModels).map(([id, modelData]) => {
                   return (
                     <Tr key={id} fontSize={"sm"}>
                       <Td>{modelData.name}</Td>
