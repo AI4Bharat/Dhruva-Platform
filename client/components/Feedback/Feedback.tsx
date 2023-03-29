5c594e4ddbb6011b7de3c8b34a4447ab0eba4750
Shanks0465 <umashanks99@gmail.com>
2023-03-29 11:29:06 +0530

    Changed Feedback Status from Modal to Toast

--- client/components/Feedback/Feedback.tsx
+++ client/components/Feedback/Feedback.tsx
@@ -6,9 +6,10 @@ import {
   Box,
   Button,
   Select,
+  useToast,
 } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { submitFeedback } from "../../api/serviceAPI";
 import { lang2label } from "../../config/config";
 
@@ -19,17 +20,16 @@ interface LanguageConfig {
   targetLanguage: string;
 }
 
-interface IFeedback
-{
-  language: string,
-  example: string,
-  rating: number,
-  comments: string,
-  service_id: string
+interface IFeedback {
+  language: string;
+  example: string;
+  rating: number;
+  comments: string;
+  service_id: string;
 }
 
 const Feedback = (props) => {
-
+  const toast = useToast();
   const mutation = useMutation(submitFeedback);
 
   const [comment, setComment] = useState("");
@@ -44,58 +44,46 @@ const Feedback = (props) => {
   const [rating, setRating] = useState(0);
   const handleRatingChange = (newRating) => setRating(newRating);
 
-
   const [modal, setModal] = useState(<></>);
   const handleSubmitFeedback = () => {
     if (comment == "" || language == "") {
-      setModal(
-        <Box
-          mt="1rem"
-          width={"100%"}
-          minH={"3rem"}
-          border={"1px"}
-          borderColor={"gray.300"}
-          background={"red.50"}
-        >
-          <Text ml="1rem" mr="1rem" mt="0.5rem" color={"red.600"}>
-            Fill all the required fields
-          </Text>
-        </Box>
-      );
+      toast({
+        title: "Fields Required",
+        description: "Fill all the required fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
     } else {
-      let request : IFeedback = {language:language, rating:rating, example:example, comments:comment, service_id:props.serviceID}
+      let request: IFeedback = {
+        language: language,
+        rating: rating,
+        example: example,
+        comments: comment,
+        service_id: props.serviceID,
+      };
       mutation.mutate(request, {
         onSuccess: (data) => {
-          setModal(
-            <Box
-              mt="1rem"
-              width={"100%"}
-              minH={"3rem"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              background={"green.50"}
-            >
-              <Text ml="1rem" mr="1rem" mt="0.5rem" color={"green.600"}>
-                Submitted Successfully
-              </Text>
-            </Box>
-          );
+          toast({
+            title: "Submitted Successfully",
+            description: "Your Feedback has been submitted.",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+          });
+          setLanguage("");
+          setComment("");
+          setExample("");
+          setRating(0);
         },
         onError: (data) => {
-          setModal(
-            <Box
-              mt="1rem"
-              width={"100%"}
-              minH={"3rem"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              background={"red.50"}
-            >
-              <Text ml="1rem" mr="1rem" mt="0.5rem" color={"red.600"}>
-                {"ERROR"}
-              </Text>
-            </Box>
-          );
+          toast({
+            title: "Error",
+            description: "An error was encountered.",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          });
         },
       });
     }
@@ -115,25 +103,31 @@ const Feedback = (props) => {
           {props.serviceID}
         </Text>
       </Box>
-      <FormControl isRequired >
-      <FormLabel mt="1rem">Language</FormLabel>
-      <Select onChange={handleLanguageChange} borderRadius={0} value={language}>
-        <option hidden defaultChecked>Select Language</option>
-        {props.serviceLanguages.map((languageConfig: LanguageConfig) => {
-          return (
-            <option
-              key={JSON.stringify(languageConfig)}
-              value={JSON.stringify(languageConfig)}
-            >
-              <Text>
-              {lang2label[languageConfig.sourceLanguage]}
-              {lang2label[languageConfig.targetLanguage]?" -> ":""}
-              {lang2label[languageConfig.targetLanguage]}
-              </Text>
-            </option>
-          );
-        })}
-      </Select>
+      <FormControl isRequired>
+        <FormLabel mt="1rem">Language</FormLabel>
+        <Select
+          onChange={handleLanguageChange}
+          borderRadius={0}
+          value={language}
+        >
+          <option hidden defaultChecked>
+            Select Language
+          </option>
+          {props.serviceLanguages.map((languageConfig: LanguageConfig) => {
+            return (
+              <option
+                key={JSON.stringify(languageConfig)}
+                value={JSON.stringify(languageConfig)}
+              >
+                <Text>
+                  {lang2label[languageConfig.sourceLanguage]}
+                  {lang2label[languageConfig.targetLanguage] ? " -> " : ""}
+                  {lang2label[languageConfig.targetLanguage]}
+                </Text>
+              </option>
+            );
+          })}
+        </Select>
       </FormControl>
       <FormControl isRequired mt="1rem">
         <FormLabel>Comments</FormLabel>
