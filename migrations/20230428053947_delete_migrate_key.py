63e120efa4ed2cce52b625451fa2763ceabb053a
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-04-28 05:45:12 +0530

    Added support for running db migrations. Also removed buildcontext from compose files.

---
+++ migrations/20230428053947_delete_migrate_key.py
@@ -0,0 +1,9 @@
+from mongodb_migrations.base import BaseMigration
+
+
+class Migration(BaseMigration):
+    def upgrade(self):
+        pass
+
+    def downgrade(self):
+        pass
---
+++ server/migrate.py
@@ -0,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+from mongodb_migrations.cli import MigrationManager
+from mongodb_migrations.config import Configuration, Execution
+
+load_dotenv()
+
+
+def parse_connection_string(conn_str: str):
+    new_conn_str = conn_str.replace("mongodb://", "")
+    parts = new_conn_str.split(":")
+    username = parts[0]
+    (password, _) = parts[1].split("@")
+
+    return {"mongo_username": username, "mongo_password": password}
+
+
+connection_string = os.environ["DB_CONNECTION_STRING"]
+execution = (
+    Execution.MIGRATE
+    if os.environ["MIGRATION_ACTION"] == "migrate"
+    else Execution.DOWNGRADE
+)
+auth_details = parse_connection_string(connection_string)
+
+config = {
+    "mongo_url": connection_string,
+    "mongo_database": "dhruva",
+    "mongo_migrations_path": "migrations",
+    "metastore": "migrations",
+    "execution": execution,
+    **auth_details,
+}
+
+manager = MigrationManager(Configuration(config))
+manager.run()
