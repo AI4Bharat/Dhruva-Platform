7adf612205241b43657305f21ec4da45b9f5459b
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-05-11 12:00:59 +0530

    Corrected order of params

--- server/auth/api_key_type_authorization_provider.py
+++ server/auth/api_key_type_authorization_provider.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, Request, status
 from schema.auth.common import ApiKeyType
 
 
-def ApiKeyTypeAuthorizationProvider(required_type: ApiKeyType, request: Request):
+def ApiKeyTypeAuthorizationProvider(request: Request, required_type: ApiKeyType):
     if ApiKeyType[request.state.api_key_type] != required_type:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
--- server/auth/role_authorization_provider.py
+++ server/auth/role_authorization_provider.py
@@ -9,7 +9,7 @@ from schema.auth.common import RoleType
 
 
 def RoleAuthorizationProvider(
-    roles: List[RoleType], request: Request, db: Database = Depends(AppDatabase)
+    request: Request, roles: List[RoleType], db: Database = Depends(AppDatabase)
 ):
     user_collection = db["user"]
     user: Dict[str, Any] = user_collection.find_one(
