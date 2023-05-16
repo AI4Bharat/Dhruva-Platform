112678465f62bef883936176451d68a8ab686328
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-05-16 12:00:46 +0530

    Ignored api key type auth check if auth type is auth token

--- server/auth/api_key_type_authorization_provider.py
+++ server/auth/api_key_type_authorization_provider.py
@@ -1,5 +1,6 @@
-from fastapi import HTTPException, Request, status
+from fastapi import Header, HTTPException, Request, status
 
+from auth.token_type import TokenType
 from schema.auth.common import ApiKeyType
 
 
@@ -7,7 +8,10 @@ class ApiKeyTypeAuthorizationProvider:
     def __init__(self, required_type: ApiKeyType):
         self.required_type = required_type
 
-    def __call__(self, request: Request):
+    def __call__(self, request: Request, x_auth_source: TokenType = Header()):
+        if x_auth_source == TokenType.AUTH_TOKEN:
+            return
+
         if ApiKeyType[request.state.api_key_type] != self.required_type:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
