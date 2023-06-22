dfc0c5e74bb8582cb889f4678002accf769e518a
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-06-22 15:38:56 +0530

    Corrected feedback timestamp migration to migrate unix timestamp to seconds

--- server/migrations/20230620161503_correct_feedback_timestamp.py
+++ server/migrations/20230620161503_correct_feedback_timestamp.py
@@ -4,8 +4,8 @@ from mongodb_migrations.base import BaseMigration
 class Migration(BaseMigration):
     def upgrade(self):
         result = self.db.feedback.update_many(
-            {"feedbackTimeStamp": {"$lte": 9999999999}},
-            {"$mul": {"feedbackTimeStamp": 1000}},
+            {"feedbackTimeStamp": {"$gt": 9999999999}},
+            {"$mul": {"feedbackTimeStamp": 0.001}},
         )
         print(
             f"Acknowledged: {result.acknowledged}, Updated Count: {result.modified_count}"
