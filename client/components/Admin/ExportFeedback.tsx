e2437325f07e060bf6eb4897dffbe611b5436ee4
Yash Kale <inferno080.com@gmail.com>
2023-06-23 15:17:22 +0530

    added serviceid to export feedback

--- client/components/Admin/ExportFeedback.tsx
+++ client/components/Admin/ExportFeedback.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { VStack, Button, Box, Text, useToast, HStack, Stack } from '@chakra-ui/react';
+import { VStack, Button, Box, Text, useToast, HStack, Stack, FormLabel, Select } from '@chakra-ui/react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import { apiInstance, dhruvaAPI } from '../../api/apiConfig';
+import { useQuery } from '@tanstack/react-query';
+import { listServices } from '../../api/serviceAPI';
 
 const ExportFeedback = () => {
     const smallscreen = useMediaQuery("(max-width: 1080px)");
     const [FromDate, setFromDate] = useState(null);
+    const [selectedServiceID, setSelectedServiceID] = useState(null);
     const [ToDate, setToDate] = useState(null);
     const toast = useToast();
     const [FromUnix, setFromUnix] = useState<number>(null);
     const [ToUnix, setToUnix] = useState<number>(null);
-
+    const { data: serviceslist } = useQuery(["services"], () => listServices());
     useEffect(() => {
         if (FromDate) {
           setFromUnix(Math.round(FromDate.getTime() / 1000));
@@ -50,8 +53,8 @@ const ExportFeedback = () => {
             }
             else
             {
-            // const url = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/services/feedback/export?fromDate=${FromUnix}&toDate=${ToUnix}`;
-            const url = `https://api.dhruva.co/services/feedback/export?fromDate=${FromUnix}&toDate=${ToUnix}`;
+            // const url = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/services/feedback/export?serviceId=${selectedServiceID}&fromDate=${FromUnix}&toDate=${ToUnix}`;
+            const url = `https://api.dhruva.co/services/feedback/export?serviceId=${selectedServiceID}&fromDate=${FromUnix}&toDate=${ToUnix}`;
             apiInstance
               .get(url, {
                 responseType: 'blob',
@@ -126,7 +129,7 @@ const ExportFeedback = () => {
         <DatePicker
           selected={FromDate}
           onChange={handleFrom}
-          customInput={<Box as="input" width={smallscreen?"200%":"100%"} px={2} py={1} borderWidth="1px" borderColor="black" borderRadius="0" />}
+          customInput={<Box as="input" width={smallscreen?"100%":"100%"} px={2} py={1} borderWidth="1px" borderColor="gray.50" borderRadius="0" />}
           showTimeSelect
           dateFormat="MMMM d, yyyy h:mm aa"
         />
@@ -137,16 +140,40 @@ const ExportFeedback = () => {
           selected={ToDate}
           onChange={handleTo}
           showTimeSelect
-          customInput={<Box as="input" width={smallscreen?"160%":"100%"} px={2} py={1} borderWidth="1px" borderColor="black" borderRadius="0" />}
+          customInput={<Box as="input" width={smallscreen?"100%":"100%"} px={2} py={1} borderWidth="1px" borderColor="gray.50" borderRadius="0" />}
           dateFormat="MMMM d, yyyy h:mm aa"
         />
         </HStack>
         </Stack>
+        <br/>        <br/>
+        <Text fontSize="lg" fontWeight="bold">
+          Select&nbsp;Service:
+        </Text>
+        <br/>
+        <HStack>
+              <Text>Service:</Text>
+              <Select
+                background={"white"}
+                borderRadius={0}
+                value={selectedServiceID}
+                minWidth="15rem"
+                onChange={(e) => {
+                  setSelectedServiceID(e.target.value);
+                }}
+              >
+                <option value="">None</option>
+                {serviceslist?.map((s: any) => {
+                  return <option value={s.serviceId}>{s.name}</option>;
+                })}
+              </Select>
+        </HStack>
         </Box>
+        <br/>
         <Button colorScheme="orange" onClick={handleClick}>
         Download CSV
         </Button>
         </VStack>
+        <br/><br/><br/>
         </Box>
     </>
   )
