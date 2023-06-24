218f7bb855bd80efa83cbabb7cc4a7f5e3059c46
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-06-24 18:12:12 +0530

    Added comments to migration

--- server/migrations/20230620161503_correct_feedback_timestamp.py
+++ server/migrations/20230620161503_correct_feedback_timestamp.py
@@ -2,6 +2,13 @@ from mongodb_migrations.base import BaseMigration
 
 
 class Migration(BaseMigration):
+    """
+    Currently some of the feedback timestamps stored in the db are in seconds,
+    and the remaining are in milliseconds. 
+
+    This migration converts all the timestamps in milliseconds to seconds.
+    """
+
     def upgrade(self):
         result = self.db.feedback.update_many(
             {"feedbackTimeStamp": {"$gt": 9999999999}},
