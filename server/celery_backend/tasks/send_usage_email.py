f75b425a5c2bf70dbdaa440890e50c129caff3bb
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-07-24 10:10:43 +0530

    Query db and write all rows to csv

--- server/celery_backend/tasks/send_usage_email.py
+++ server/celery_backend/tasks/send_usage_email.py
@@ -5,8 +5,13 @@ import smtplib
 import ssl
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from typing import List
 
+import sqlalchemy
 from dotenv import load_dotenv
+from metering_database import ApiKey, engine
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 from ..celery_app import app
 
@@ -14,17 +19,36 @@ load_dotenv()
 
 
 def get_csv():
-    mylist = [["a", "b"], ["c", "d"]]
     file = io.StringIO()
-    csv.writer(file).writerows(mylist)
+    c = csv.writer(file)
+
+    stmt = select(
+        ApiKey.api_key_id,
+        ApiKey.api_key_name,
+        ApiKey.user_id,
+        ApiKey.user_email,
+        sqlalchemy.func.sum(ApiKey.usage),
+        sqlalchemy.func.count(ApiKey.usage),
+    ).group_by(
+        ApiKey.api_key_id,
+        ApiKey.api_key_name,
+        ApiKey.user_id,
+        ApiKey.user_email,
+    )
+
+    with Session(engine) as session:
+        rows = session.scalars(stmt).all()
+        for row in rows:
+            c.writerow(row)
+
     file.seek(0)
     return file
 
 
-def create_email():
+def create_email(sender: str, recipients: str):
     message = MIMEMultipart()
-    message["From"] = os.environ["USAGE_EMAIL_SENDER"]
-    message["To"] = os.environ["USAGE_EMAIL_RECIPIENTS"].split(",")
+    message["From"] = sender
+    message["To"] = recipients
     message["Subject"] = "Weekly Dhruva Usage Report"
     part = MIMEBase("application", "octet-stream")
     part.set_payload(get_csv().read())
@@ -39,14 +63,14 @@ def create_email():
 @app.task(name="send.usage.email")
 def send_usage_email() -> None:
     """Send usage email to listed emails"""
-    email_list = os.environ["USAGE_EMAIL_RECIPIENTS"].split(",")
+    email_list = os.environ["USAGE_EMAIL_RECIPIENTS"]
     sender = os.environ["USAGE_EMAIL_SENDER"]
     smtp_username = os.environ["SMTP_USERNAME"]
     smtp_password = os.environ["SMTP_PASSWORD"]
 
-    context = ssl.create_default_context()
+    email = create_email(sender, email_list)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(os.environ["SMTP_SERVER"], 465, context=context) as server:
         server.login(smtp_username, smtp_password)
-        email = create_email()
-        server.sendmail(sender, email_list, email)
+        server.sendmail(sender, email_list.split(","), email)
