085e9fde1ca4fac84cc66c019f46638cf7ae4499
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-07-25 21:16:46 +0530

    Added itn and punctuation post processors for asr

--- server/module/services/gateway/inference_gateway.py
+++ server/module/services/gateway/inference_gateway.py
@@ -1,15 +1,15 @@
+import time
 import traceback
-from typing import Any
+from typing import Any, Optional
 
 import gevent.ssl
 import requests
 import tritonclient.http as http_client
+from custom_metrics import INFERENCE_REQUEST_COUNT, INFERENCE_REQUEST_DURATION_SECONDS
+from exception.base_error import BaseError
 from fastapi import Request
 from fastapi.logger import logger
 from numpy import block
-
-from custom_metrics import INFERENCE_REQUEST_COUNT, INFERENCE_REQUEST_DURATION_SECONDS
-from exception.base_error import BaseError
 
 from ..error import Errors
 from ..model import Service
@@ -59,47 +59,52 @@ class InferenceGateway:
         output_list: list,
         request_state: Request,
         task_type: str,
-        request_body: Any,
+        request_body: Optional[Any] = None,
     ):
-        INFERENCE_REQUEST_COUNT.labels(
-            request_state.state.api_key_name,
-            request_state.state.user_id,
-            request_body.config.serviceId,
-            task_type,
-            request_body.config.language.sourceLanguage,
-            request_body.config.language.dict().get("targetLanguage"),
-        ).inc()
+        if request_body:
+            INFERENCE_REQUEST_COUNT.labels(
+                request_state.state.api_key_name,
+                request_state.state.user_id,
+                request_body.config.serviceId,
+                task_type,
+                request_body.config.language.sourceLanguage,
+                request_body.config.language.dict().get("targetLanguage"),
+            ).inc()
 
-        with INFERENCE_REQUEST_DURATION_SECONDS.labels(
-            request_state.state.api_key_name,
-            request_state.state.user_id,
-            request_body.config.serviceId,
-            task_type,
-            request_body.config.language.sourceLanguage,
-            request_body.config.language.dict().get("targetLanguage"),
-        ).time():
-            try:
-                triton_client = http_client.InferenceServerClient(
-                    url=url,
-                    ssl=True,
-                    ssl_context_factory=gevent.ssl._create_default_https_context,  # type: ignore
-                    concurrency=20,
-                )
+        begin = time.perf_counter()
+        try:
+            triton_client = http_client.InferenceServerClient(
+                url=url,
+                ssl=True,
+                ssl_context_factory=gevent.ssl._create_default_https_context,  # type: ignore
+                concurrency=20,
+            )
 
-                # health_ctx = triton_client.is_server_ready(headers=headers)
-                # logger.info("Health ctx: {}".format(health_ctx))
-                # if not health_ctx:
-                #     raise BaseError(Errors.DHRUVA107.value, "Triton server is not ready")
-                response = triton_client.async_infer(
-                    model_name,
-                    model_version="1",
-                    inputs=input_list,
-                    outputs=output_list,
-                    headers=headers,
-                )
-                response = response.get_result(block=True, timeout=20)
+            # health_ctx = triton_client.is_server_ready(headers=headers)
+            # logger.info("Health ctx: {}".format(health_ctx))
+            # if not health_ctx:
+            #     raise BaseError(Errors.DHRUVA107.value, "Triton server is not ready")
+            response = triton_client.async_infer(
+                model_name,
+                model_version="1",
+                inputs=input_list,
+                outputs=output_list,
+                headers=headers,
+            )
+            response = response.get_result(block=True, timeout=20)
 
-            except:
-                raise BaseError(Errors.DHRUVA101.value, traceback.format_exc())
+        except:
+            raise BaseError(Errors.DHRUVA101.value, traceback.format_exc())
 
-            return response
+        end = time.perf_counter()
+        if request_body:
+            INFERENCE_REQUEST_DURATION_SECONDS.labels(
+                request_state.state.api_key_name,
+                request_state.state.user_id,
+                request_body.config.serviceId,
+                task_type,
+                request_body.config.language.sourceLanguage,
+                request_body.config.language.dict().get("targetLanguage"),
+            ).observe(end - begin)
+
+        return response
---
+++ server/module/services/service/post_processor_service.py
@@ -0,0 +1,86 @@
+import os
+from typing import Dict, List, Tuple
+
+import numpy as np
+import tritonclient.http as http_client
+from dotenv import load_dotenv
+from fastapi import Request
+
+from ..gateway import InferenceGateway
+
+load_dotenv()
+
+
+class PostProcessorService:
+    def __init__(self, inference_gateway: InferenceGateway) -> None:
+        self.inference_gateway = inference_gateway
+
+    async def run_itn(
+        self,
+        line: str,
+        language: str,
+        request_state: Request,
+    ):
+        input1 = http_client.InferInput("INPUT_TEXT", [1, 1], "BYTES")
+        input1.set_data_from_numpy(
+            np.asarray([line.encode("utf-8")]).astype("object").reshape([1, 1])
+        )
+        input2 = http_client.InferInput("LANG_ID", [1, 1], "BYTES")
+        lang_id = [language]
+        input2.set_data_from_numpy(np.asarray(lang_id).astype("object").reshape([1, 1]))
+
+        inputs = [input1, input2]
+
+        output0 = http_client.InferRequestedOutput("OUTPUT_TEXT")
+        outputs = [output0]
+
+        headers = {"Authorization": "Bearer " + os.environ["ITN_ENDPOINT_API_KEY"]}
+
+        response1 = await self.inference_gateway.send_triton_request(
+            url=os.environ["ITN_ENDPOINT"],
+            model_name="itn",
+            input_list=inputs,
+            output_list=outputs,
+            headers=headers,
+            request_state=request_state,
+            task_type="",
+        )
+
+        batch_result = response1.as_numpy("OUTPUT_TEXT")
+        res = " ".join([result.decode("utf8") for result in batch_result])
+        return res
+
+    async def run_punctuation(
+        self,
+        line: str,
+        language: str,
+        request_state: Request,
+    ):
+        input1 = http_client.InferInput("INPUT_TEXT", [1, 1], "BYTES")
+        input1.set_data_from_numpy(
+            np.asarray([line.encode("utf-8")]).astype("object").reshape([1, 1])
+        )
+        input2 = http_client.InferInput("LANG_ID", [1, 1], "BYTES")
+        lang_id = [language]
+        input2.set_data_from_numpy(np.asarray(lang_id).astype("object").reshape([1, 1]))
+
+        inputs = [input1, input2]
+
+        output0 = http_client.InferRequestedOutput("OUTPUT_TEXT")
+        outputs = [output0]
+
+        headers = {"Authorization": "Bearer " + os.environ["ITN_ENDPOINT_API_KEY"]}
+
+        response1 = await self.inference_gateway.send_triton_request(
+            url=os.environ["ITN_ENDPOINT"],
+            model_name="punctuation",
+            input_list=inputs,
+            output_list=outputs,
+            headers=headers,
+            request_state=request_state,
+            task_type="",
+        )
+
+        batch_result = response1.as_numpy("OUTPUT_TEXT")
+        res = " ".join([result.decode("utf8") for result in batch_result])
+        return res
