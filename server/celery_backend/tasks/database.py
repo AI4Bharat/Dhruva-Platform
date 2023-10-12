c41a4a4d7ef86ccc24d8d624955ae6d7fca6c541
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-10-12 16:39:17 +0530

    Fixed db connection error in celery backend

--- server/celery_backend/tasks/database.py
+++ server/celery_backend/tasks/database.py
@@ -9,14 +9,8 @@ from pymongo.database import Database
 load_dotenv(override=True)
 
 db_clients = {
-    # For some reason with the latest changes the quotes in the fields in .env are getting sent to python
-    # Removing the quotes from the string in .env without removing them from the URL
     "app": pymongo.MongoClient(os.environ["APP_DB_CONNECTION_STRING"]),
 }
-
-if os.environ.get("LOG_REQUEST_RESPONSE_DATA_FLAG", None):
-    db_clients["log"] = pymongo.MongoClient(os.environ["LOG_DB_CONNECTION_STRING"])
-
 
 def AppDatabase() -> Database:
     mongo_db = db_clients["app"][os.environ["APP_DB_NAME"]]
@@ -24,8 +18,6 @@ def AppDatabase() -> Database:
 
 
 def LogDatastore() -> BlobServiceClient:
-    # mongo_db = db_clients["log"]["dhruva"]
-
     token_credential = DefaultAzureCredential()
     blob_service_client = BlobServiceClient(
         account_url=f'https://{os.environ.get("BLOB_STORE")}.blob.core.windows.net',
