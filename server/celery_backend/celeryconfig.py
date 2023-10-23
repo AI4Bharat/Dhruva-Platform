17decc456af3fd88bc36bc64c25b02ce3c2d5772
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-10-23 11:44:47 +0530

    Added imports for celery and removed celery-flower from requirements

--- server/celery_backend/celeryconfig.py
+++ server/celery_backend/celeryconfig.py
@@ -9,3 +9,11 @@ load_dotenv()
 broker_url = os.environ[
     "CELERY_BROKER_URL"
 ]
+
+imports = (
+    "celery_backend.tasks.log_data",
+    "celery_backend.tasks.heartbeat",
+    "celery_backend.tasks.upload_feedback_dump",
+    "celery_backend.tasks.send_usage_email",
+    "celery_backend.tasks.push_metrics",
+)
