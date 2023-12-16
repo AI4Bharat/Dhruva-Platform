b2ae95eaf6b2c7a072d8b04843cd2a7dcb74bdf7
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-12-16 16:28:59 +0530

    Added suport for redis ssl connection

--- server/cache/app_cache.py
+++ server/cache/app_cache.py
@@ -12,4 +12,5 @@ def get_cache_connection():
         port=os.environ.get("REDIS_PORT"),
         db=os.environ.get("REDIS_DB"),
         password=os.environ.get("REDIS_PASSWORD"),
+        ssl=os.environ.get("REDIS_SECURE"),
     )
